46e59417ab8dda18bfbb93dcf051d617986fd2f0
ExitusKalma <jukka.niemi73@hotmail.fi>
2023-12-10 10:54:38 +0200

    feat: day 10 part 2

--- day_10/part2/solution.go
+++ day_10/part2/solution.go
@@ -20,31 +20,116 @@ type pipe struct {
 var maze = make(map[vector]pipe)
 var start vector
 
-var queue = []vector{}
+var fillMatrix = make(map[vector]int)
+
+var size vector
+
+var queue []vector
+var fillMatrixQueue []vector
 
 func main() {
 	lines := readInput("day_10/input.txt")
 	readMaze(lines)
-
 	updateDistances(maze[start])
 
 	for len(queue) > 0 {
-		var location vector = queue[0]
+		var location = queue[0]
 		updateDistances(maze[location])
 		queue = queue[1:]
 	}
 
-	fmt.Println(findLongestDistance())
-}
+	size = vector{len(lines[0]) - 1, len(lines)}
+	buildFillMatrix()
 
-func findLongestDistance() int {
-	longestDistance := 0
-	for _, pipe := range maze {
-		if pipe.distance > longestDistance {
-			longestDistance = pipe.distance
+	for y := 0; y < size.y*2; y++ {
+		fill(vector{0, y})
+		fill(vector{size.x*2 - 1, y})
+	}
+	for x := 0; x < size.y*2; x++ {
+		fill(vector{x, 0})
+		fill(vector{x, size.y*2 - 1})
+	}
+
+	//count empties in coordinates divided by 2
+	enclosed := 0
+	for y := 0; y < size.y*2; y++ {
+		for x := 0; x < size.x*2; x++ {
+			if x%2 == 0 && y%2 == 0 {
+				if fillMatrix[vector{x, y}] == 0 {
+					enclosed++
+				}
+			}
+
 		}
 	}
-	return longestDistance
+	fmt.Println("enclosed:", enclosed)
+
+	//debug print fillMatrix
+	//for y := 0; y < size.y; y++ {
+	//	for x := 0; x < size.x; x++ {
+	//		position := vector{x * 2, y * 2}
+	//
+	//		if fillMatrix[position] == 1 {
+	//			fmt.Print("X")
+	//		} else if fillMatrix[position] == 2 {
+	//			fmt.Print("O")
+	//		} else {
+	//			fmt.Print(".")
+	//		}
+	//	}
+	//	fmt.Println()
+	//}
+
+}
+
+func fill(startPos vector) {
+	fillMatrixQueue = append(fillMatrixQueue, startPos)
+	for len(fillMatrixQueue) > 0 {
+		currentPos := fillMatrixQueue[0]
+		fillMatrixQueue = fillMatrixQueue[1:]
+		if fillMatrix[currentPos] > 0 {
+			continue
+		}
+		fillMatrix[currentPos] = 2
+		if currentPos.y > 0 {
+			fillMatrixQueue = append(fillMatrixQueue, vector{currentPos.x, currentPos.y - 1})
+		}
+		if currentPos.y < size.y*2 {
+			fillMatrixQueue = append(fillMatrixQueue, vector{currentPos.x, currentPos.y + 1})
+		}
+		if currentPos.x > 0 {
+			fillMatrixQueue = append(fillMatrixQueue, vector{currentPos.x - 1, currentPos.y})
+		}
+		if currentPos.x < size.x*2 {
+			fillMatrixQueue = append(fillMatrixQueue, vector{currentPos.x + 1, currentPos.y})
+		}
+	}
+}
+
+func buildFillMatrix() {
+	for y := 0; y < size.y; y++ {
+		for x := 0; x < size.x; x++ {
+			if currentPipe, ok := maze[vector{x, y}]; ok {
+				if currentPipe.distance == -1 {
+					continue
+				}
+				fillMatrix[vector{x * 2, y * 2}] = 1
+				if isConnectedTo(currentPipe, vector{x, y - 1}) {
+					fillMatrix[vector{x * 2, y*2 - 1}] = 1
+				}
+				if isConnectedTo(currentPipe, vector{x, y + 1}) {
+					fillMatrix[vector{x * 2, y*2 + 1}] = 1
+				}
+				if isConnectedTo(currentPipe, vector{x + 1, y}) {
+					fillMatrix[vector{x*2 + 1, y * 2}] = 1
+				}
+				if isConnectedTo(currentPipe, vector{x - 1, y}) {
+					fillMatrix[vector{x*2 - 1, y * 2}] = 1
+				}
+			}
+		}
+	}
+
 }
 
 func updateDistances(currentPipe pipe) {
@@ -81,17 +166,20 @@ func getReverseNeighbours(target pipe) []vector {
 			neighbours = append(neighbours, sourcePipe.location)
 		}
 	}
+	// check down
 	if sourcePipe, ok := getPipe(vector{target.location.x, target.location.y + 1}); ok {
 		if isConnectedTo(sourcePipe, start) {
 			neighbours = append(neighbours, sourcePipe.location)
 		}
 	}
 	if sourcePipe, ok := getPipe(vector{target.location.x + 1, target.location.y}); ok {
+
 		if isConnectedTo(sourcePipe, start) {
 			neighbours = append(neighbours, sourcePipe.location)
 		}
 	}
 	if sourcePipe, ok := getPipe(vector{target.location.x - 1, target.location.y}); ok {
+
 		if isConnectedTo(sourcePipe, start) {
 			neighbours = append(neighbours, sourcePipe.location)
 		}
@@ -119,7 +207,6 @@ func readMaze(lines []string) {
 			}
 			if char == "S" {
 				start = vector{x, y}
-
 			}
 			if char == "|" {
 				directions = []vector{
